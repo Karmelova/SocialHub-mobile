7f3968500167d06d5a6607c5ef280f46bd2dbea7
Karmelova <kamila.weronika.marcinek@gmail.com>
2024-05-28 00:24:33 +0200

    searchbar prototype

--- socialhub/components/common/Searchbar.tsx
+++ socialhub/components/common/Searchbar.tsx
@@ -1,23 +1,36 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput } from 'react-native';
 import { Text } from 'react-native-elements';
 
-
 function Searchbar() {
-    
-    return (
-        <View
-        style={{
-          flex: 1,
-          backgroundColor: "white",
-          borderRadius: 8,
-          marginHorizontal: 10,
-          padding: 8,
-        }}
-      >
-        <Text>Search Bar</Text>
-      </View>
-    );
-  }
-  
-  export default Searchbar;
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearch = (text) => {
+    // Update the searchQuery state as the user types
+    setSearchQuery(text);
+
+    // Perform search operations based on the updated searchQuery
+    console.log('Search Query:', text);
+    // You can implement your search logic here, such as filtering data or making API calls
+  };
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: 'white',
+        borderRadius: 8,
+        marginHorizontal: 10,
+        padding: 3,
+      }}
+    >
+      <TextInput
+        onChangeText={handleSearch} // Call handleSearch whenever the text changes
+        value={searchQuery}
+        placeholder="Search..."
+      />
+    </View>
+  );
+}
+
+export default Searchbar;
